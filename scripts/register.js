c7804daae33b95061fef4e5a2f2496383c5cf34f
Nishat Quyoum <nishatq9006@gmail.com>
2023-12-03 18:21:22 -0800

    added error handling

--- scripts/register.js
+++ scripts/register.js
@@ -5,6 +5,49 @@ window.signup = async function (){
     const password = document.getElementById("password").value;
     const fullname = document.getElementById("fullname").value;
     const profile = document.getElementById("image").files[0];
+    const allowedDomains = ["@gmail.com", "@yahoo.com", "@outlook.com"]; // Define allowed domains
+    // Check if email contains an allowed domain
+    const isValidDomain = allowedDomains.some(domain => email.includes(domain));
+    const emailField = document.getElementById("email");
+    const emailError = document.getElementById("emailError")
+    //checks for valid username
+    const isValidUsername = /[a-zA-Z].*[a-zA-Z]/.test(fullname);
+    const usernameField = document.getElementById("fullname");
+    const usernameError = document.getElementById("usernameError");
+      // Check if the password meets the criteria
+    const isValidPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(password);
+    const passwordField = document.getElementById("password");
+    const passwordError = document.getElementById("passwordError");
+     
+   
+    if (!isValidPassword) {
+        // If the password is invalid, show an error message
+        passwordField.classList.add("is-invalid");
+        passwordError.style.display = "block";
+        return;
+    }
+
+    // If the password is valid, remove error message and styling
+    passwordField.classList.remove("is-invalid");
+    passwordError.style.display = "none";
+
+    
+    if (!isValidUsername) {
+        // If the username doesn't contain at least 2 letters, show an error message
+        usernameField.classList.add("is-invalid");
+        usernameError.style.display = "block";
+        return;
+    }
+
+    // If the username is valid, remove error message and styling
+    usernameField.classList.remove("is-invalid");
+    usernameError.style.display = "none";
+    
+    if (!isValidDomain) {
+        // If the domain is not valid, show an error message
+        document.getElementById("email").classList.add("is-invalid");
+        return;
+    }
     // Check if any input field is empty or profile image is not uploaded
     if(email == "" || password == "" || fullname == "" || profile == undefined){
         alert("All the inputs required");
@@ -37,7 +80,7 @@ window.signup = async function (){
             default :
             document.getElementsByClassName("btn-outline-dark")[0].style.display = "inline-block"  
             document.getElementsByClassName("btn-outline-dark")[1].style.display = "none" 
-            document.getElementById("error").innerText = "unknown error occured"
+            document.getElementById("error").innerText = "Error: Please enter valid inputs."
             
         }
         console.log(e.code)
