3b854d3d3957ed643b4e3ab3f3bbf1d2a74448d4
Nishat Quyoum <nishatq9006@gmail.com>
2023-12-03 18:21:36 -0800

    added search bar for searcing users

--- scripts/userList.js
+++ scripts/userList.js
@@ -1,34 +1,62 @@
 import { getUsersFromDb, userLogout, checkRoom, auth } from "/config/firebase.js";
 
-// Function to fetch users from the database and display them in the UI
 getUsers();
 async function getUsers() {
     const users = await getUsersFromDb(); // Retrieve users from the database
-    for (let item of users) {
-        // Display each user's information in the UI
-        document.getElementById("users").innerHTML += `
-            <li class="list-group-item d-flex justify-content-between align-items-start text-light bg-dark">
-                <img class="user-img bg-light" src='${item.imageUrl}'> 
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold">${item.fullname}</div>
-                    ${item.email}
-                </div>
-                <span class="badge bg-primary rounded-pill">
-                    <button style="border:none;background:transparent;color:white" onclick="initiateChat('${item.userId}')">chat</button>
-                </span>
-            </li>`;
-    }
-}
+    const userList = document.getElementById("users");
 
-// Function to initiate a chat with a specific user and redirect to the chat page
-window.initiateChat = async function (friendId) {
-    const chatRoom = await checkRoom(friendId); // Check if a chat room exists with the friend
-    window.location.href = `chat.html?id=${chatRoom.id}`; // Redirect to the chat page with the chat room ID as a query parameter
-}
+    // Display each user's information in the UI
+    users.forEach(item => {
+      const userListItem = document.createElement('li');
+      userListItem.classList.add('list-group-item');
+      userList.innerHTML += ` 
+      
+      <div class="user-info" onclick="initiateChat('${item.userId}')">
+      <li class="list-group-item">
+      <img class="user-img" src='${item.imageUrl}'> 
+      <div class="profile-details">
+          <div class="profileListName">${item.fullname}</div>
+      </div>
+  </div>
+ </li> 
+      `;
+    });
+    
 
+    // Function to filter users based on search input
+    document.getElementById("searchInput").addEventListener("input", function() {
+        const searchValue = this.value.toLowerCase();
+        userList.innerHTML = ""; // Clear the list
+
+        // Filter users based on search input
+        const filteredUsers = users.filter(user =>
+            user.fullname.toLowerCase().includes(searchValue) || user.email.toLowerCase().includes(searchValue)
+        );
+
+        // Display filtered users
+        filteredUsers.forEach(item => {
+            userList.innerHTML += `
+            <div class="user-info" onclick="initiateChat('${item.userId}')">
+            <li class="list-group-item">
+            <img class="user-img" src='${item.imageUrl}'> 
+            <div class="profile-details">
+                <div class="profileListName">${item.fullname}</div>
+            </div>
+        </div>
+       </li> `;
+        });
+    });
+}
 // Function to handle user logout
 window.logout = function () {
     userLogout();
-    window.location.replace("register.html"); // Redirect to the registration page after logout
+    window.location.replace("login.html"); // Redirect to the registration page after logout
 }
+// Function to initiate a chat with a specific user and redirect to the chat page
+window.initiateChat = async function (friendId) {
+    const chatRoom = await checkRoom(friendId); // Check if a chat room exists with the friend
+    window.location.href = `userList.html?id=${chatRoom.id}`; // Redirect to the chat page with the chat room ID as a query parameter
+}
+
+
 
