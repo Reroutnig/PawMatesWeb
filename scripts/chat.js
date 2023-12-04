880cc1707f852b20a39260bff30cbf14adaa1da5
Nishat Quyoum <nishatq9006@gmail.com>
2023-12-03 18:20:44 -0800

    displayed users in chat screen

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,5 +1,5 @@
 //import functions from firebase.js
-import { sendMessageToDb,getMessagesFromDb,auth} from "/config/firebase.js";
+import { sendMessageToDb,getMessagesFromDb,auth,getUsersFromDb} from "/config/firebase.js";
 
 
 //gets room ID
@@ -10,7 +10,10 @@ function getRoom(){
     const id = urlParams.get('id');
     console.log(id); //logs the id
     myroomId = id //store the id in a variable
+
+   
 }
+
 
 
 
@@ -38,35 +41,90 @@ window.sendMessage = function(){
         text.value = ""
     }
 }
-//retrieves and display messages from firestore database
-getMessages()
 
-function getMessages(){
+async function getMessages() {
     // Get messages from the database based on the current room ID
-    getMessagesFromDb(myroomId,(messages)=>{
+    getMessagesFromDb(myroomId, async (messages) => {
+         // Get the current scroll position
+        const chatArea = document.querySelector('.messageBox');
+        const currentScrollPos = chatArea.scrollTop;
         const messageEle = document.getElementById("messages");
         messageEle.innerHTML = '';
-         // Loop through retrieved messages and display them in the UI
-        for(let item of messages){
+        
+        const users = await getUsersFromDb(); // Fetch all users' details
+        const currentUser = users.find(user => user.userId === auth.currentUser.uid);
+       
+                
+  //displays logged in user in chat navBar     
+        if (currentUser) {
+            const chatUserDiv = document.querySelector('.chatUser');
+            chatUserDiv.innerHTML = `
+                
+                <div class="profilechatlocation">
+                <img class="user-imgNavbar" src="${currentUser.imageUrl}" alt="${currentUser.fullname}">
+                    <div class="profileNavbarName">${currentUser.fullname}</div>
+                </div>
+            `;
+        }
+
+        // Loop through retrieved messages and display them in the UI
+        for (let item of messages) {
+            
             var color;
             var time_align;
-            if(item.userId === auth.currentUser.uid){
-                color = "orange"
-                time_align = "right"
+            
+            if (item.userId === auth.currentUser.uid) {
+                color = "orange";
+                time_align = "right";
+              
+                // Construct HTML for displaying messages with current user's profile
+                messageEle.innerHTML += `
+                    <div class="message-${color}">
+                        <div class="message-bubble">
+                            <div class="user-profile">
+                                <div class="user-details">
+                                    <p class="profileName">${currentUser.fullname}</p>
+                                </div>
+                                <img class="user-imgchatorg" src="${currentUser.imageUrl}" alt="${currentUser.fullname}" />
+                            </div>
+                            <p class="message-content">${item.text}</p>
+                            <div class="message-timestamp-${time_align}">${new Date(item.createdAt).toLocaleTimeString()}</div>
+                        </div>
+                    </div>`
+                    
+                    ;
+            } else {
+                color = "blue";
+                time_align = "left";
+                
+                // Find the friend's information
+                const friend = users.find(user => user.userId === item.userId);
+                
+                // Construct HTML for displaying messages with friend's profile
+                messageEle.innerHTML += `
+                    <div class="message-${color}">
+                        <div class="message-bubble">
+                            <div class="user-profile">
+                                <div class="user-details">
+                                    <p class="profileName">${friend.fullname}</p>
+                                </div>
+                                <img class="user-imgchat" src="${friend.imageUrl}" alt="${friend.fullname}" />
+                            </div>
+                            <p class="message-content">${item.text}</p>
+                            <div class="message-timestamp-${time_align}">${new Date(item.createdAt).toLocaleTimeString()}</div>
+                        </div>
+                    </div>`;
+                    
+                     
             }
-            else{
-                color = "blue"
-                time_align = "left"
-                console.log(auth.currentUser.uid)
-            }
-            // Construct HTML for displaying messages with styling
-            messageEle.innerHTML += `    <div class="message-${color}">
-            <p class="message-content">${item.text}</p>
-            <div class="message-timestamp-${time_align}">${new Date(item.createdAt).toLocaleTimeString()}</div>
-        </div>`
+            // Set the scroll position back to where it was
+            chatArea.scrollTop = currentScrollPos;
+            
         }
-    })
-
+      
+    });
+    
 }
+//retrieves and display messages from firestore database
+getMessages();
 
-   
