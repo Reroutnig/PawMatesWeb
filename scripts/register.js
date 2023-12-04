1e73456785689919869ee52903ac493ae2b2e041
Nishat Quyoum <nishatq9006@gmail.com>
2023-12-03 23:24:00 -0800

    remove code

--- scripts/register.js
+++ scripts/register.js
@@ -1,16 +1,4 @@
-import {addUserToDb, checkUser,signUpFirebase, checkLoggedInUser,uploadImage} from "/config/firebase.js"
-// Check if the user is logged in before allowing access to the page
-checkLoggedInUser()
-    .then((user) => {
-        // User is logged in, allow access to the page
-        console.log("User is logged in");
-        // Add your logic for allowing access here
-    })
-    .catch(() => {
-        // User is not logged in, redirect to the login page
-        console.log("User is not logged in, redirecting to login page");
-        window.location.href = "login.html"; // Redirect to login page
-    });
+import {addUserToDb, checkUser,signUpFirebase,uploadImage} from "/config/firebase.js"
 
 window.signup = async function (){
     const email = document.getElementById("email").value;
